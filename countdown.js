edae591e7860cf78af1953a5934b285d7f067437
git-ebetts <ebettsmail@gmail.com>
2015-10-13 15:53:57 -0400

    first commit: Solution Level 1

--- countdown.js
+++ countdown.js
@@ -1,5 +1,15 @@
-function countdown(seconds){
-  // ...
-}
+//SOLUTION LEVEL 1
 
+var x;
+function countdown(seconds){
+    x = seconds;
+    var intervalID = setInterval(printSeconds, 1000);
+    function printSeconds() {
+        console.log(x);
+        x --;
+        if (x < 0) {
+        	clearInterval(intervalID);
+        }
+    }
+}
 countdown(5);
